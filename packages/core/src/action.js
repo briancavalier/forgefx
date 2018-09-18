2e6fdbdb4e3bb98f9fa01844342a352e810e8aea
Brian Cavalier <brian@briancavalier.com>
2018-09-18 08:59:11 -0400

    Remove unused param

--- packages/core/src/action.js
+++ packages/core/src/action.js
@@ -16,7 +16,7 @@ export function * chain <E, A, F, B> (a: Action<E, A>, f: A => Action<F, B>): Ac
 }
 
 export const timeout = <A> (ms: number, action: Action<Async, A>): Action<Async, Either<void, A>> =>
-  race(delay(ms, undefined), action)
+  race(delay(ms), action)
 
 export const par = <E, F, A, B, C> (f: (A, B) => C, aa: Action<E, A>, ab: Action<F, B>): Action<E | F, C> =>
   // $FlowFixMe Why doesn't flow like the type here?
