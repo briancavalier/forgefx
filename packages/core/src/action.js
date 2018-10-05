3035740d32c1efe16ef5319aac9f92dae6df9a3c
Brian Cavalier <brian@briancavalier.com>
2018-10-05 18:24:56 -0400

    Par propagates failure, simplify all, traverse, apply

--- packages/core/src/action.js
+++ packages/core/src/action.js
@@ -8,7 +8,7 @@ export function * map <E, A, B> (f: A => B, aa: Action<E, A>): Action<E, B> {
   return f(yield * aa)
 }
 
-export const apply = <E, A, F, B> (af: Action<E, A => B>, aa: Action<F, A>): Action<E | F, B> =>
+export const apply = <E, A, B> (af: Action<E, A => B>, aa: Action<E, A>): Action<E, B> =>
   par((f, a) => f(a), af, aa)
 
 export const withHandler = <H: {}, E, A> (handler: H, action: Action<Effect<H> | E, A>): Action<E, A> =>
@@ -17,41 +17,11 @@ export const withHandler = <H: {}, E, A> (handler: H, action: Action<Effect<H> |
 export const timeout = <A> (ms: number, action: Action<Async, A>): Action<Async, Either<void, A>> =>
   race(delay(ms), action)
 
-export const par = <E, F, A, B, C> (f: (A, B) => C, aa: Action<E, A>, ab: Action<F, B>): Action<E | F, C> =>
-  call(context => async(runPar(f, aa, ab, context.next, context.handler)))
+export const par = <E, A, B, C> (f: (A, B) => C, aa: Action<E, A>, ab: Action<E, B>): Action<E, C> =>
+  map(([a, b]) => f((a.value: any), (b.value: any)), all([map(left, aa), map(right, ab)]))
 
-const runPar = <E, F, A, B, C> (f: (A, B) => C, aa: Action<E, A>, ab: Action<F, B>, step: Step<C>, handler: E | F): Cancel => {
-  let a: ?A
-  let b: ?B
-  let remaining = 2
-  const c = ab => {
-    if (ab.right) b = ab.value
-    else a = ab.value
-
-    if (--remaining === 0) step(f(((a: any): A), ((b: any): B)))
-  }
-  const c1 = runAction(c, map(left, aa), handler)
-  const c2 = runAction(c, map(right, ab), handler)
-  const cancel = cancelBoth(c1, c2)
-  return cancel
-}
-
-export const race = <E, A, F, B> (aa: Action<E, A>, ab: Action<F, B>): Action<E | F, Either<A, B>> =>
-  call(context => async(runRace(aa, ab, context.next, context.handler)))
-
-const runRace = <E, F, A, B> (aa: Action<E, A>, ab: Action<F, B>, step: Step<Either<A, B>>, handler: E | F): Cancel => {
-  const c = ab => {
-    ab.right ? c1() : c2()
-    step(ab)
-  }
-  const c1 = runAction(c, map(left, aa), handler)
-  const c2 = runAction(c, map(right, ab), handler)
-  const cancel = cancelBoth(c1, c2)
-  return cancel
-}
-
-export const all = <E, A> (action: Action<E, Either<Error, A>>[]): Action<E, Either<Error, A[]>> =>
-  traverse(a => a, action)
+export const all = <E, A> (action: Action<E, A>[]): Action<E, Either<Error, A[]>> =>
+  traverse(right, action)
 
 export const traverse = <E, A, B> (f: A => Either<Error, B>, a: Action<E, A>[]): Action<E, Either<Error, B[]>> =>
   call(context => async(runTraverse(f, a, context.next, context.handler)))
@@ -70,5 +40,19 @@ const runTraverse = <E, A, B> (f: A => Either<Error, B>, a: Action<E, A>[], step
     if (--remaining === 0) step(right(results))
   }, action, handler))
   const cancel = cancelAll(cancels)
+  return cancel
+}
+
+export const race = <E, A, F, B> (aa: Action<E, A>, ab: Action<F, B>): Action<E | F, Either<A, B>> =>
+  call(context => async(runRace(aa, ab, context.next, context.handler)))
+
+const runRace = <E, F, A, B> (aa: Action<E, A>, ab: Action<F, B>, step: Step<Either<A, B>>, handler: E | F): Cancel => {
+  const c = ab => {
+    ab.right ? c1() : c2()
+    step(ab)
+  }
+  const c1 = runAction(c, map(left, aa), handler)
+  const c2 = runAction(c, map(right, ab), handler)
+  const cancel = cancelBoth(c1, c2)
   return cancel
 }
